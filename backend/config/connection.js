9c47a3621380f91af4d8f946667689a7ce8aa322
Tio Meko <61386052+TioMeko@users.noreply.github.com>
2024-06-10 10:36:00 -0400

    allow connection to databsae

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,14 +1,26 @@
-import { Sequelize } from 'sequelize';
 import 'dotenv/config';
+import mysql from 'mysql2/promise';
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
-  }
-);
+// Using a pool to manage multiple connections
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-export default sequelize;
+// Verify the connection
+pool.getConnection()
+    .then(conn => {
+        console.log('Connected successfully to the database!');
+        conn.release();
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+
+export default pool;
